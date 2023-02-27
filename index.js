d8420aa9c058c9e02f4ef122b5bc002705cc64c0
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-26 19:21:45 -0700

    updated Schema db

---
+++ index.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+function starterPrompt(){
+
+}
+
+function viewEmployees() {
+
+}
+
+
+function viewEmployeesByManager(){
+
+}
+
+
+function viewEmployeesByDepartment (){
+
+}
+
+
+function addEmployee(){
+
+}
+
+function employeeData(){}
---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+
+CREATE TABLE department (
+     id INT NOT NULL PRIMARY KEY
+    name VARCHAR(50) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    itle VARCHAR(50) NULL,
+    salary VARCHAR(50) NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(50) NULL,
+last_name VARCHAR(50) NULL,
+role_id INT NULL,
+manager_id INT NULL,
+PRIMARY KEY (id)
+);
+
